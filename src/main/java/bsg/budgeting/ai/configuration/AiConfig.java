6c68463dc8096968445f01a61623e9b3f8ea67cf
Your Name <you@example.com>
2026-08-20 13:50:09 -0300

    Cria arquivo system.st para armazenar o prompt usado em defaultSystem.

--- src/main/java/bsg/budgeting/ai/configuration/AiConfig.java
+++ src/main/java/bsg/budgeting/ai/configuration/AiConfig.java
@@ -3,25 +3,20 @@ package bsg.budgeting.ai.configuration;
 import bsg.budgeting.ai.tool.CategoryTool;
 import bsg.budgeting.ai.tool.TransactionTool;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class AiConfig {
 
+    @Value("classpath:/prompts/system.st")
+    private Resource systemPrompt;
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder, TransactionTool transactionTool, CategoryTool categoryTool) {
-        return builder
-                .defaultSystem("""
-                        Você é um assistente financeiro.
-                        Sua tarefa é extrair dados de transações e usar as ferramentas disponíveis para manipular transações.
-                        Ao registrar uma transação, busque as categorias disponíveis e escolha a que melhor se enquadrar.
-                        Caso a transação não se encaixe em nenhuma categoria, crie uma nova.
-                        Ao cadastrar uma categoria, certifique-se de não usar algo muito extenso. A ideia é ser simples e objetivo.
-                        Informe ao usuário qual categoria foi utilizada e se ela foi criada agora.
-                        
-                        Sua resposta será utilizada para gerar um áudio, portanto nada de "decorações" de texto (negrito, itálico, etc).
-                        """)
+        return builder.defaultSystem(systemPrompt)
                 .defaultTools(transactionTool, categoryTool)
                 .build();
     }
